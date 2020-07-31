18653f5c477f2918e01b4572a5450ba5de81af13
Jon Haase <haase1020@gmail.com>
2020-07-31 19:34:42 -0400

    tree_paths_sum.py solution for CodeSignal challenge

---
+++ tree_paths_sum.py
@@ -0,0 +1,19 @@
+# CodeSignal code challenge for unit assessment
+# prompt: given the root of a binary tree where each node contains
+# an integer, determine the sum of all the integer values
+# in the tree.
+
+# following code provided: binary trees are already defined with this interface:
+class Tree(object):
+    def __init__(self,  x):
+        self.value = x
+        self.left = None
+        self.right = None
+
+# this is the code I added:
+
+
+def tree_paths_sum(root):
+    if(root == None):
+        return 0
+    return (root.value + tree_paths_sum(root.left) + tree_paths_sum(root.right))
