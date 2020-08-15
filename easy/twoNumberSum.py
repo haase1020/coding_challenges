7d5ec5a09d20d3fcdc275e424b61f92750adf9cd
Jon Haase <haase1020@gmail.com>
2020-08-15 05:24:51 -0400

    remove solutions not working for twoNumberSum in python3

--- easy/twoNumberSum.py
+++ easy/twoNumberSum.py
@@ -2,28 +2,6 @@
 # with hashtable O(n) for time and space complexity
 # can also use two pointers (if array is sorted) starting at both ends:time is 0(n log n), and space 0(1)
 
-# # double for loop option: 0(n^2) time | 0(1) space
-# def twoNumberSum(array, targetSum):
-#     for i in range(len(array) - 1):
-#         firstNum = array[i]
-#         for j in range(i + 1, len(array)):
-#             secondNum = array[j]
-#             if firstNum + secondNum == targetSum:
-#                 return [firstNum, secondNum]
-#     return []
-
-
-# # hashtable option: 0(n) time | 0(n) space
-# def twoNumberSum(array, targetSum):
-#     nums = {}
-#     for num in array:
-#         potentialMatch = targetSum - num
-#         if potentialMatch in nums:
-#             return [potentialMatch, num]
-#         else:
-#             nums[num] = True
-#     return []
-
 
 # two pointer option: 0(nlog(n)) time | 0(1) space
 def twoNumberSum(array, targetSum):
