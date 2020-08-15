cf75f3554c451e9013856f8e44419ed16f250259
Jon Haase <haase1020@gmail.com>
2020-08-15 05:23:04 -0400

    solution for twoNumberSum problem in python3

--- easy/twoNumberSum.py
+++ easy/twoNumberSum.py
@@ -27,15 +27,22 @@
 
 # two pointer option: 0(nlog(n)) time | 0(1) space
 def twoNumberSum(array, targetSum):
+    newArray = []
     array.sort()
+    print(array)
     left = 0
     right = len(array) - 1
     while left < right:
         currentSum = array[left] + array[right]
         if currentSum == targetSum:
-            return [array[left], array[right]]
+            newArray.append([array[left], array[right]])
+            right -= 1
         elif currentSum < targetSum:
             left += 1
         elif currentSum > targetSum:
             right -= 1
-    return []
+    return newArray
+
+
+array = [3, 5, 2, -4, 8, 11]
+print(twoNumberSum(array, 7))
