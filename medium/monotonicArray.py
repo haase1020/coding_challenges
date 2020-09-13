f7b2808e45823945c6aaeec2f591262e3e5c108b
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-09-13 15:55:23 -0400

    first solution for monotonicArray.py

--- medium/monotonicArray.py
+++ medium/monotonicArray.py
@@ -2,6 +2,26 @@
 # an array is said to be monotonic if its elements, from L to R, are entirely non-increasing or entirely non-decreasing.
 # Note that empty arrays and arras of one element are monotonic.
 
-def isMonotonic(array):
 
-    pass
+# 0(n) time | 0(1) space
+def isMonotonic(array):
+    if len(array) <= 2:
+        return True
+    direction = array[1] - array[0]
+    for i in range(2, len(array)):
+        if direction == 0:
+            direction = array[i] - array[i-1]
+            continue
+        if breaksDirection(direction, array[i-1], array[i]):
+            return False
+    return True
+
+
+def breaksDirection(direction, previousInt, currentInt):
+    difference = currentInt - previousInt
+    if direction > 0:
+        return difference < 0
+    return difference > 0
+
+
+print(isMonotonic([1, -2, 3, 4, 5]))
