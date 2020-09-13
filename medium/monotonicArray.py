89583e6de1db6a562451f0191eb35726629f2b5b
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-09-13 15:34:02 -0400

    create monotonicArray.py file

---
+++ medium/monotonicArray.py
@@ -0,0 +1,7 @@
+# write a function that takes in an array of ints and returns a boolean representing whether the array is monotonic
+# an array is said to be monotonic if its elements, from L to R, are entirely non-increasing or entirely non-decreasing.
+# Note that empty arrays and arras of one element are monotonic.
+
+def isMonotonic(array):
+
+    pass
