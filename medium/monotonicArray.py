a76243e553e7bea40b64c56e252b02a84f9add94
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-09-13 15:58:39 -0400

    second solution for monotonicArray.py

--- medium/monotonicArray.py
+++ medium/monotonicArray.py
@@ -3,25 +3,38 @@
 # Note that empty arrays and arras of one element are monotonic.
 
 
+# # 0(n) time | 0(1) space
+# def isMonotonic(array):
+#     if len(array) <= 2:
+#         return True
+#     direction = array[1] - array[0]
+#     for i in range(2, len(array)):
+#         if direction == 0:
+#             direction = array[i] - array[i-1]
+#             continue
+#         if breaksDirection(direction, array[i-1], array[i]):
+#             return False
+#     return True
+
+
+# def breaksDirection(direction, previousInt, currentInt):
+#     difference = currentInt - previousInt
+#     if direction > 0:
+#         return difference < 0
+#     return difference > 0
+
+
 # 0(n) time | 0(1) space
 def isMonotonic(array):
-    if len(array) <= 2:
-        return True
-    direction = array[1] - array[0]
-    for i in range(2, len(array)):
-        if direction == 0:
-            direction = array[i] - array[i-1]
-            continue
-        if breaksDirection(direction, array[i-1], array[i]):
-            return False
-    return True
+    isNonDecreasing = True
+    isNonIncreasing = True
+    for i in range(1, len(array)):
+        if array[i] < array[i - 1]:
+            isNonDecreasing = False
+
+        if array[i] > array[i - 1]:
+            isNonIncreasing = False
+    return isNonDecreasing or isNonIncreasing
 
 
-def breaksDirection(direction, previousInt, currentInt):
-    difference = currentInt - previousInt
-    if direction > 0:
-        return difference < 0
-    return difference > 0
-
-
-print(isMonotonic([1, -2, 3, 4, 5]))
+print(isMonotonic([1, 2, 3, 4, 5]))
