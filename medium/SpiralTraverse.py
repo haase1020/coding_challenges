9ab741473ffd3fa6d85e6beb98dd21dc58291c6c
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-09-21 07:10:08 -0400

    create spiralTraverse file

---
+++ medium/SpiralTraverse.py
@@ -0,0 +1,7 @@
+# write a function that takes in an n x m two dimenstional array (that can be square-shaped when n==m) and returns
+# a one-dminesional array of all the array's elements in spiral order.
+# Spiral order starts at the top left corner of the two dimentional array, goes to the right, and process in a spiral pattern all the way
+# until every element has been visited.
+
+
+def spiralTraverse(array):
