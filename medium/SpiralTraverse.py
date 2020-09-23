d173882b7b0d688d083e232c19db10df29648849
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-09-23 06:12:41 -0400

    update spiralTraverse.py

--- medium/SpiralTraverse.py
+++ medium/SpiralTraverse.py
@@ -7,52 +7,66 @@
 # 0(n) time | 0(n) space/ or constant space
 # iterative solution
 
-# def spiralTraverse(array):
-
-#     result = []
-#     startRow, endRow = 0, len(array) - 1
-#     startCol, endCol = 0, len(array[0]) - 1
-
-#     while startRow <= endRow and startCol <= endCol:
-#         if startRow == endRow:
-#             break  # avoid double-counting single row
-#         for col in range(startCol, endCol + 1):
-#             result.append(array[startRow][col])
-#         for row in range(startRow + 1, endRow + 1):
-#             result.append(array[row][endCol])
-
-#         for col in reversed(range(startCol, endCol)):
-#             result.append(array[endRow][col])
-#         for row in reversed(range(startRow + 1, endRow)):
-#             result.append(array[row][startCol])
-
-#         startRow += 1
-#         endRow -= 1
-#         startCol += 1
-#         endCol -= 1
-
-#     return result
-
-
-# 0(n) time | 0(n) space
 def spiralTraverse(array):
+
     result = []
-    spiralFill(array, 0, len(array) - 1, 0, len(array[0]) - 1, result)
+    startRow, endRow = 0, len(array) - 1
+    startCol, endCol = 0, len(array[0]) - 1
+
+    while startRow <= endRow and startCol <= endCol:
+
+        for col in range(startCol, endCol + 1):
+            result.append(array[startRow][col])
+        for row in range(startRow + 1, endRow + 1):
+            result.append(array[row][endCol])
+
+        for col in reversed(range(startCol, endCol)):
+            if startRow == endRow:
+                break  # avoid double-counting single row
+            result.append(array[endRow][col])
+        for row in reversed(range(startRow + 1, endRow)):
+            if startCol == endCol:
+                break
+            result.append(array[row][startCol])
+
+        startRow += 1
+        endRow -= 1
+        startCol += 1
+        endCol -= 1
+
     return result
 
 
-def spiralFill(array, startRow, endRow, startCol, endCol, result):
-    if startRow > endRow or startCol > endCol:
-        return
-    for col in range(startCol, endCol + 1):
-        result.append(array[startRow][col])
-    for row in range(startRow + 1, endRow + 1)
-    result.append(array[row][endCol])
+# 0(n) time | 0(n) space
+# def spiralTraverse(array):
+#     result = []
+#     spiralFill(array, 0, len(array) - 1, 0, len(array[0]) - 1, result)
+#     return result
 
-    for col in reversed(range(startCol, endCol)):
-        result.append(array[endRow][col])
-    for row in reversed(range(startRow + 1, endRow)):
-        result.append(array[row][startCol])
 
-    spiralFill(array, startRow + 1, endRow - 1,
-               startCol + 1, endCol - 1, result)
+# def spiralFill(array, startRow, endRow, startCol, endCol, result):
+#     if startRow > endRow or startCol > endCol:
+#         return
+#     for col in range(startCol, endCol + 1):
+#         result.append(array[startRow][col])
+#     for row in range(startRow + 1, endRow + 1):
+#         result.append(array[row][endCol])
+
+#     for col in reversed(range(startCol, endCol)):
+#         if startRow ==endRow:
+#             break
+#         result.append(array[endRow][col])
+#     for row in reversed(range(startRow + 1, endRow)):
+#         if startCol == endCol:
+#             break
+#         result.append(array[row][startCol])
+
+#     spiralFill(array, startRow + 1, endRow - 1,
+#                startCol + 1, endCol - 1, result)
+
+
+print(spiralTraverse(
+    [[1, 2, 3, 4],
+     [12, 13, 14, 5],
+     [11, 16, 15, 6],
+     [10, 9, 8, 7]]))
