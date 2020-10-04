fcf83fcd0fdf28498b1fb9675dae65b3f8d8a21f
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-10-04 05:08:15 -0400

    twoSum.js optimized solution

--- zeroToMastery/twoSum.js
+++ zeroToMastery/twoSum.js
@@ -4,17 +4,39 @@
 // return null if there is no solution
 // only one pair of numbers that will equal target
 
-//brute force solution:
-const findToSum = function (nums, target) {
+//brute force solution | 0(n^2) time complexity | 0(1) space complexity:
+// const findTwoSum = function (nums, target) {
+//   for (let a = 0; a < nums.length; a++) {
+//     const numberToFind = target - nums[a];
+//     for (let b = a + 1; b < nums.length; b++) {
+//       if (nums[b] === numberToFind) {
+//         return [a, b];
+//       }
+//     }
+//   }
+//   return null;
+// };
+
+// console.log(findTwoSum([1, 2, 7, 4, 5], 3));
+
+// optimized solution | 0(n) time complexity | 0(n) space in worst case
+const findTwoSum = function (nums, target) {
+  const numsMap = {};
+  console.log({ numsMap });
   for (let a = 0; a < nums.length; a++) {
-    const numberToFind = target - nums[a];
-    for (let b = a + 1; b < nums.length; b++) {
-      if (nums[b] === numberToFind) {
-        return [a, b];
-      }
+    console.log({ a, value: nums[a] });
+    const currentMapVal = numsMap[nums[a]];
+    console.log({ currentMapVal });
+    if (currentMapVal >= 0) {
+      return [currentMapVal, a];
+    } else {
+      const numberToFind = target - nums[a];
+      console.log({ numberToFind });
+      numsMap[numberToFind] = a;
+      console.log({ numsMap });
     }
   }
   return null;
 };
 
-console.log(findToSum([1, 2, 7, 4, 5], 3));
+console.log(findTwoSum([1, 2, 7, 4, 5], 3));
