72c5db5daf5f9ac727c3c6dee287a5e638c65c2f
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-10-04 11:54:58 -0400

    brute force approach for trappingRainWater

--- zeroToMastery/trappingRainWater.js
+++ zeroToMastery/trappingRainWater.js
@@ -1,3 +1,33 @@
 // Given n non-negative integers representing an elevation map where the width of each bar is 1,
 // compute how much water it is able to trap after raining.
 // assume all integers are positive
+
+//brute force solution
+
+const elevationArray = [0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2];
+
+const getTrappedRainwater = function (heights) {
+  let totalWater = 0;
+
+  for (let p = 0; p < heights.length; p++) {
+    let leftP = p,
+      rightP = p,
+      maxLeft = 0,
+      maxRight = 0;
+    while (leftP >= 0) {
+      maxLeft = Math.max(maxLeft, heights[leftP]);
+      leftP--;
+    }
+    while (rightP < heights.length) {
+      maxRight = Math.max(maxRight, heights[rightP]);
+      rightP++;
+    }
+    const currentWater = Math.min(maxLeft, maxRight) - heights[p];
+    if (currentWater >= 0) {
+      totalWater += currentWater;
+    }
+  }
+  return totalWater;
+};
+
+console.log(getTrappedRainwater(elevationArray));
