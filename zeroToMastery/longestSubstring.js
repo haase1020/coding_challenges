74334ffc3c4e70d1cc8bf9639c6be828951ca2b0
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-10-05 19:32:26 -0400

    add text

--- zeroToMastery/longestSubstring.js
+++ zeroToMastery/longestSubstring.js
@@ -1,3 +1,4 @@
 // given a string s, find the length of the longest substring without repeating characters.
+// leetcode #3 Longest Substring Without Repeating Characters
 
 var lengthOfLongestSubstring = function (s) {};
