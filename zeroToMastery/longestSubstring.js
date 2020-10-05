46fe1c002eafcd1e9098cc931392e528454fc2b6
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-10-05 19:53:08 -0400

    brute force solution for longestSubstring.js

--- zeroToMastery/longestSubstring.js
+++ zeroToMastery/longestSubstring.js
@@ -2,6 +2,26 @@
 // leetcode #3 Longest Substring Without Repeating Characters
 
 //brute force
-var lengthOfLongestSubstring = function (s) {};
+var lengthOfLongestSubstring = function (s) {
+  if (s.length <= 1) return s.length;
+
+  let longest = 0;
+
+  for (let left = 0; left < s.length; left++) {
+    let seenChars = {},
+      currentLength = 0;
+    for (let right = left; right < s.length; right++) {
+      const currentChar = s[right];
+      if (!seenChars[currentChar]) {
+        currentLength++;
+        seenChars[currentChar] = true;
+        longest = Math.max(longest, currentLength);
+      } else {
+        break;
+      }
+    }
+  }
+  return longest;
+};
 
 console.log(lengthOfLongestSubstring('abcbdca'));
