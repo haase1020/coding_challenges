d92a48a76cf656a9930247dbab34aaf8e0f76e5b
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-10-07 05:59:08 -0400

    begin almostPalindrome.js problem

---
+++ zeroToMastery/almostPalindrome.js
@@ -0,0 +1,5 @@
+/*
+🔎 LeetCode question #680: valid palindrome II
+        Given a string, determine if it is almost a palindrome. A string is almost a palindrome if it becomes
+        a palindrome by removing 1 letter. Consider only alphanumeric characters and ignore case sensitivity.
+*/
