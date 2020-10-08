6c446fe772551ea28b7b102278905ae883f3b26f
DESKTOP-293EMP1\jonjh <haase1020@gmail.com>
2020-10-08 04:38:41 -0400

    solution for almostPalindrome.js

--- zeroToMastery/almostPalindrome.js
+++ zeroToMastery/almostPalindrome.js
@@ -2,4 +2,44 @@
 🔎 LeetCode question #680: valid palindrome II
         Given a string, determine if it is almost a palindrome. A string is almost a palindrome if it becomes
         a palindrome by removing 1 letter. Consider only alphanumeric characters and ignore case sensitivity.
+        A palindrome is considered almost a palindrome.
+
+        ✅ step 1: Test cases --> "raceacar" True, "abccdba" True, "abcdefdba" False, "" True, "a" True, "ab" True
 */
+
+/* 
+🙋 solution
+time complexity 0(n) | space complexity 0(1)
+*/
+
+const validSubPalindrome = function (s, left, right) {
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+};
+
+const isValidPalindrome = function (s) {
+  s = s.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+
+  let left = 0;
+  right = s.length - 1;
+
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      return (
+        validSubPalindrome(s, left + 1, right) ||
+        validSubPalindrome(s, left, right - 1)
+      );
+    }
+    left++;
+    right--;
+  }
+  return true;
+};
+
+console.log(isValidPalindrome('raceacar'));
